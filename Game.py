e6f39a92da16e7758d379645ec520b14f5ae9585
Ahmed Ali Mostafa <119663082+MatMan04@users.noreply.github.com>
2025-01-26 17:54:24 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -5,85 +5,79 @@ print("ROCK,PAPER,SCISSORS")
 x=0
 y=0
 z=0
+#random move for computer
+def opponent(move):
+    if move==1:
+        return "Rock"
+    elif move==2:
+        return "Paper"
+    elif move==3:
+        return "Scissors"
 for i in range (100):
+#will count number of wins,lossesand ties
     print(str(x)+" Wins, "+str(y)+" Losses, "+str(z)+" Ties")
     print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
+#player type move
     move=input()
+#player choose rock
     if move == "r":
-        print("rock versus...")
-        random_num=rd.randint(1,3)
-        if random_num==1:
-            opponent="p"
-            print("Paper")
-        elif random_num==2:
-            opponent="r"
-            print("Rock")
-        elif random_num==3:
-            opponent="s"
-            print("Sissors")
-        if opponent==move:
+        print("Rock versus...")
+        r=rd.randint(1,3)
+        move_2=opponent(r)
+        print(move_2)
+        if move_2=="Rock":
             print("It is a tie")
             z=z+1
             continue
-        elif opponent=="p":
+        elif move_2=="Paper":
             print("You lose")
             y=y+1
             continue
-        elif opponent=="s":
+        elif move_2=="Scissors":
             print("you win!")
             x=x+1
             continue
+#player choose paper
     elif move=="p":
-        print("paper versus...")
-        random_num=rd.randint(1,3)
-        if random_num==1:
-            opponent="p"
-            print("Paper")
-        elif random_num==2:
-            opponent="r"
-            print("Rock")
-        elif random_num==3:
-            opponent="s"
-            print("Sissors")
-        if opponent==move:
+        print("Paper versus...")
+        r=rd.randint(1,3)
+        move_2=opponent(r)
+        print(move_2)
+        if move_2=="Paper":
             print("It is a tie")
             z=z+1
             continue
-        elif opponent=="s":
+        elif move_2=="Scissors":
             print("You lose")
             y=y+1
             continue
-        elif opponent=="r":
+        elif move_2=="Rock":
             print("you win!")
             x=x+1
             continue
+#player choose scissors
     elif move =="s":
         print("Sissors versus...")
-        random_num=rd.randint(1,3)
-        if random_num==1:
-            opponent="p"
-            print("Paper")
-        elif random_num==2:
-            opponent="r"
-            print("Rock")
-        elif random_num==3:
-            opponent="s"
-            print("Sissors")
-        if opponent==move:
+        r=rd.randint(1,3)
+        move_2=opponent(r)
+        print(move_2)         
+        if move_2=="Scissors":
             print("It is a tie")
             z=z+1
             continue
-        elif opponent=="r":
+        elif move_2=="Rock":
             print("You lose")
             y=y+1
             continue
-        elif opponent=="p":
+        elif move_2=="Paper":
             print("you win!")
             x=x+1
             continue
+#player choose to quit
     elif move =="q":
         print("Good Bye")
         sys.exit()
+#player type another words
     else:
         print("Please type one of r, p, s, or q.")
     
